9c8ed14e5fd04a542a2e112b942cdb6988ee1311
Philip Jasionowski <philip.jasionowski@gmail.com>
2024-09-11 19:24:23 -0400

    Added generate Grid function and bound it to button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// TODO:    enable them to be hovered (generate this iteratively)
+// TODO:    Scale buttons to said width and height
+// TODO:    Creat event listener for button
 // TODO:    enable the user to select how many divs they want (grab a number and square
 //          it to the nearest int)
 
@@ -11,31 +12,27 @@ const button = document.createElement("button");
 button.textContent = "Clear";
 button.setAttribute("id", "clearButton");
 buttonHolder.append(button);
-//testing down below
 
-// const div = document.createElement("div");
+button.addEventListener("click", () => generateGrid(100));
 
-// div.setAttribute("id", "testDiv");
-// div.style.backgroundColor = "lightblue";
-// root.appendChild(div);
-
-for (let y = 0; y < size; y++) {
-  const flexContainer = document.createElement("div");
-  flexContainer.setAttribute("id", `flexContainer${y}`);
-  flexContainer.style.display = "flex";
-  root.appendChild(flexContainer);
-  for (let x = 0; x < size; x++) {
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute("id", `div${y * 16 + x}`);
-    newDiv.setAttribute("class", "pixel");
-    newDiv.style.width = "5px";
-    newDiv.style.height = "5px";
-    newDiv.style.margin = "1px";
-    newDiv.style.backgroundColor = "lightblue";
-    newDiv.addEventListener("mouseover", () => {
-      newDiv.style.backgroundColor = "black";
-    });
-    // newDiv.textContent = `I am Div ${y * 16 + x}`;
-    flexContainer.appendChild(newDiv);
+function generateGrid(size) {
+  for (let y = 0; y < size; y++) {
+    const flexContainer = document.createElement("div");
+    flexContainer.setAttribute("id", `flexContainer${y}`);
+    flexContainer.setAttribute("class", "flexContainer");
+    flexContainer.style.display = "flex";
+    root.appendChild(flexContainer);
+    for (let x = 0; x < size; x++) {
+      const newDiv = document.createElement("div");
+      newDiv.setAttribute("id", `div${y * 16 + x}`);
+      newDiv.setAttribute("class", "pixel");
+      newDiv.style.margin = "1px";
+      newDiv.style.backgroundColor = "lightblue";
+      newDiv.addEventListener("mouseover", () => {
+        newDiv.style.backgroundColor = "black";
+      });
+      // newDiv.textContent = `I am Div ${y * 16 + x}`;
+      flexContainer.appendChild(newDiv);
+    }
   }
 }
