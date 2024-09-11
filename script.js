6f457800e64ac73984787fe31865fe2e778a187f
Philip Jasionowski <philip.jasionowski@gmail.com>
2024-09-11 18:54:50 -0400

    Added a button

--- script.js
+++ script.js
@@ -2,8 +2,15 @@
 // TODO:    enable the user to select how many divs they want (grab a number and square
 //          it to the nearest int)
 
-const root = document.getElementById("root");
+let size = 16;
 
+const root = document.getElementById("root");
+const buttonHolder = document.getElementById("buttonHolder");
+
+const button = document.createElement("button");
+button.textContent = "Clear";
+button.setAttribute("id", "clearButton");
+buttonHolder.append(button);
 //testing down below
 
 // const div = document.createElement("div");
@@ -12,15 +19,15 @@ const root = document.getElementById("root");
 // div.style.backgroundColor = "lightblue";
 // root.appendChild(div);
 
-for (let y = 0; y < 16; y++) {
+for (let y = 0; y < size; y++) {
   const flexContainer = document.createElement("div");
   flexContainer.setAttribute("id", `flexContainer${y}`);
   flexContainer.style.display = "flex";
   root.appendChild(flexContainer);
-  for (let x = 0; x < 16; x++) {
+  for (let x = 0; x < size; x++) {
     const newDiv = document.createElement("div");
     newDiv.setAttribute("id", `div${y * 16 + x}`);
-    newDiv.setAttribute("class", "pixel]");
+    newDiv.setAttribute("class", "pixel");
     newDiv.style.width = "5px";
     newDiv.style.height = "5px";
     newDiv.style.margin = "1px";
